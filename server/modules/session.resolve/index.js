4d89131dc15e5a3e4c4ea0ade4a2da1bb7897dae
Selkin Vitaly <contact@selkinvitaly.ru>
2016-06-23 23:31:50 +0300

    set httponly for cookies

--- server/modules/session.resolve/index.js
+++ server/modules/session.resolve/index.js
@@ -4,10 +4,7 @@ const session = require("koa-generic-session");
 const mongooseStore = require("koa-session-mongoose");
 
 const sessionStore = mongooseStore.create({
-  model: "Session",
-  cookie: {
-    httpOnly: false
-  }
+  model: "Session"
 });
 
 module.exports = function(app) {
